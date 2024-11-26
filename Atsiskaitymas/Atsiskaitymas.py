a24deb2c7dac1d7bb71a87b8719812d561eea392
JustasBertauskas <justasbert@gmail.com>
2024-11-27 01:01:59 +0200

    Update Atsiskaitymas.py
    :))))

--- Atsiskaitymas/Atsiskaitymas.py
+++ Atsiskaitymas/Atsiskaitymas.py
@@ -1,1 +1,21 @@
+import time
 
+class BurbuloRusiavimas:
+    def __init__(self, sarasas):
+        self.sarasas = sarasas
+
+    def rusiuoti(self):
+        n = len(self.sarasas)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if self.sarasas[j] > self.sarasas[j+1] :
+                    self.sarasas[j], self.sarasas[j+1] = self.sarasas[j+1], self.sarasas[j]
+
+my_ist = BurbuloRusiavimas(nerusiuotas_sarasas)
+
+start_time = time.time()
+my_list.rusiuoti()
+end_time = time.time()
+
+print("Surūšiuotas sąrašas:", rusytuvas.sarasas)
+print("Rūšiavimo laikas:", end_time - start_time, "sekundės")
